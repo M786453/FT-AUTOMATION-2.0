e74d383a2f4992eacb481e755049a6e4902d3967
M786453 <ahteshamsarwar333@gmail.com>
2023-06-06 00:07:35 +0500

    Prompting user to enter hostname,username, and password

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ from SSHClient import SSHClient
 from SFTPClient import SFTPClient
 
 
+
 def main():
-    # List of destination systems
-    destinations = [
-        {"hostname": "192.168.56.101", "username": "kaushik", "password": "1234"}
-    ]
+
+    hostname = input("Enter Hostname: ")
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")
+
     """
     source_path: path of directory present on remote system
     destination_path: path where the directory will be downloaded on local system
     """
-    source_path = "C:/12345"
-    destination_path = "C:/Users/kaushik/Documents/Python/Data-Copy-Auto/Test"
+    source_path = "E:/123"
+    destination_path = "./"
 
-    # Operation
-    for destination in destinations:
-        # Create ssh connection
-        ssh_connection = SSHClient(
-            destination["hostname"], destination["username"], destination["password"]
+
+    ssh_connection = SSHClient(
+            hostname, username, password
         )
 
-        if ssh_connection.connect():  # connection successful
-            try:
-                sftp_client = SFTPClient(ssh_connection)
-                sftp_client.download_directory(source_path, destination_path)
-            finally:
-                # close ssh connection
-                ssh_connection.disconnect()
+    if ssh_connection.connect():  # connection successful
+        try:
+            sftp_client = SFTPClient(ssh_connection)
+            sftp_client.download_directory(source_path, destination_path)
+        finally:
+            # close ssh connection
+            ssh_connection.disconnect()
 
 if __name__ == "__main__":
     main()
